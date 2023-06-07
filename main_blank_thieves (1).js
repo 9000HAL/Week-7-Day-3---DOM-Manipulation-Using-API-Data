ad68c0de1c5ff9ce43d476abdabb3ed757303856
9000HAL <gsanchezscs@gmail.com>
2023-06-07 15:39:10 -0700

    initial open commit --- SAVINGG?

---
+++ main_blank_thieves (1).js
@@ -0,0 +1,31 @@
+// connect/test js file to html
+
+// .getElementsByTagName()
+
+// .getElementsByClassName()
+
+// .getElementById()
+
+// .querySelector() (ES6 Modern Approach)
+
+// .querySelectorAll()
+
+// .innerText property: Changes the text
+
+// .innerHTML property: Allows you to write HTML code
+
+// .createElement()
+
+// NOTE: Rememeber when creating an element in JS, you need to append to your document with .append()
+
+// Javascript is an event driven language
+// We can take advantage of its events by using .addEventListener(event_type, callback function)
+
+// Option 2: using onclick attribute in HTML
+// onclick="function(event)"
+
+// Lets create a new button and append to a specfic div
+
+// adding addEventListener() to new button to add text every time its clicked
+
+// grabbing form data with .addEventListener()
