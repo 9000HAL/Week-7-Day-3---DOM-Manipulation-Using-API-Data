3fca4f1a142a59a994838ad27ebff2bc217cc08a
9000HAL <gsanchezscs@gmail.com>
2023-06-09 02:12:43 -0700

    friday am re-save

--- main_blank_thieves (1).js
+++ main_blank_thieves (1).js
@@ -29,12 +29,55 @@
 // Option 2: using onclick attribute in HTML
 // onclick="function(event)"
 
+
+
+
+
+
+
 // Lets create a new button and append to a specfic div
 
-
+// (see above example)
 
 
 
 // adding addEventListener() to new button to add text every time its clicked
 
+const btn = document.createElement('button')
+dylansDiv.append(btn)
+btn.innerText = 'Adding text'
+
+btn.addEventListener('click'), () => {
+    const pTag = document.createElement('p')
+    pTag.innerText = 'YEERRR'
+    dylansDiv.append(pTag)
+
+}
+
+
 // grabbing form data with .addEventListener()
+
+
+
+//******************************RECORDING GAP HERE******************************************************
+
+
+//HW---------------------------
+const pokeDiv = document.querySelector('.poke-div')
+
+const pokemonData = async (name) => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon--------see RECORDINGGGGGGGGGGGGGGGG`)
+    const data = await response.json()
+    console.log(data)
+    pokeDiv.innerHTML = `
+    
+    see recodingg------------------
+    
+    
+    
+    `
+
+    
+
+
+}
